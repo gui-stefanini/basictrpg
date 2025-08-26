1f4879612584715b06cecc33aa1c9d896515abc1
gui-stefanini <guistefanini1@gmail.com>
2025-08-26 04:30:26 -0300

    Ver(Exp: 1.0)
    Ready to Export

--- Scripts/MenuScripts/main_menu.gd
+++ Scripts/MenuScripts/main_menu.gd
@@ -10,14 +10,32 @@ extends Control
 #     REFERENCES     #
 ######################
 @export var SelectionMenuScene: PackedScene
+@export var ResolutionButton: OptionButton
 
 ######################
 #     SCRIPT-WIDE    #
 ######################
 
+var Resolutions: Array[Vector2i] = [
+	Vector2i(1600, 960),
+	Vector2i(1280, 768),
+	Vector2i(640, 384)
+]
+
 ##############################################################
 #                      2.0 Functions                         #
 ##############################################################
+
+func SetResolutions():
+	for resolution in Resolutions:
+		ResolutionButton.add_item("%d x %d" % [resolution.x, resolution.y])
+	
+	# Set the default selection to match the current window size
+	var current_size = DisplayServer.window_get_size()
+	for i in range(Resolutions.size()):
+		if Resolutions[i] == current_size:
+			ResolutionButton.select(i)
+			return
 
 ##############################################################
 #                      3.0 Signal Functions                  #
@@ -32,3 +50,9 @@ func _on_quit_button_pressed() -> void:
 ##############################################################
 #                      4.0 Godot Functions                   #
 ##############################################################
+
+func _on_resolution_button_item_selected(index: int) -> void:
+	DisplayServer.window_set_size(Resolutions[index])
+
+func _ready() -> void:
+	SetResolutions()
