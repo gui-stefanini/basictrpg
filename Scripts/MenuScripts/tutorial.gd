5a2566e31353260c43028469a1fd4f92fb2559b0
gui-stefanini <guistefanini1@gmail.com>
2025-08-29 21:33:03 -0300

    Ver 1.0.2.2 - Pre
    Tutorial menu impl

--- Scripts/MenuScripts/tutorial.gd
+++ Scripts/MenuScripts/tutorial.gd
@@ -1,4 +1,5 @@
 extends Control
+
 ##############################################################
 #                      0.0 Signals                           #
 ##############################################################
@@ -9,54 +10,23 @@ extends Control
 ######################
 #     REFERENCES     #
 ######################
-@export var SelectionMenuScene: PackedScene
-@export var TutorialScene: PackedScene
-@export var ResolutionButton: OptionButton
-
+#Can't have 2+ scenes setting each other as Packed variables
+@export_file("*.tscn") var MainMenuPath: String
 ######################
 #     SCRIPT-WIDE    #
 ######################
-
-var Resolutions: Array[Vector2i] = [
-	Vector2i(1600, 960),
-	Vector2i(1280, 768),
-	Vector2i(640, 384)
-]
 
 ##############################################################
 #                      2.0 Functions                         #
 ##############################################################
 
-func SetResolutions():
-	for resolution in Resolutions:
-		ResolutionButton.add_item("%d x %d" % [resolution.x, resolution.y])
-	
-	# Set the default selection to match the current window size
-	var current_size = DisplayServer.window_get_size()
-	for i in range(Resolutions.size()):
-		if Resolutions[i] == current_size:
-			ResolutionButton.select(i)
-			return
-
 ##############################################################
 #                      3.0 Signal Functions                  #
 ##############################################################
 
-func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_packed(SelectionMenuScene)
-
-func _on_tutorial_button_pressed() -> void:
-	get_tree().change_scene_to_packed(TutorialScene)
-
-func _on_quit_button_pressed() -> void:
-	get_tree().quit()
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file(MainMenuPath)
 
 ##############################################################
 #                      4.0 Godot Functions                   #
 ##############################################################
-
-func _on_resolution_button_item_selected(index: int) -> void:
-	DisplayServer.window_set_size(Resolutions[index])
-
-func _ready() -> void:
-	SetResolutions()
